56d1ffd2134c281f1ae85e796d423f60f3ba8704
ACameno <125655585+ACameno@users.noreply.github.com>
2023-02-24 12:33:11 +0100

    Add files via upload

--- sucesion/Sucesion.java
+++ sucesion/Sucesion.java
@@ -2,11 +2,6 @@ package sucesion;
 
 import javax.swing.JOptionPane;
 
-/**
- * @author aaloca3
- *
- */
-
 public class Sucesion {
 
 	public static void main(String[] args) {
@@ -14,27 +9,28 @@ public class Sucesion {
 		long x=Long.parseLong(JOptionPane.showInputDialog("Introduzca el término x:","Introduzca un número entero"));
 		long n=Long.parseLong(JOptionPane.showInputDialog("Introduzca el término n:","Introduzca un número entero"));
 
-		long factorial=0;
-		long potencia=0;
+		double factorial=0;
+		double potencia=0;
 		double resultado=0;
 		
 		
 		//calculo del factorial y la potencia
 		for (int i=0; i<=n; i++) {
 			if (i==0) { //cuando estamos en 0!
-				
 				factorial=1;
 				potencia=1;
-				resultado=potencia/factorial; //añadimos al resulado cuando x^0/0!
-				
+				resultado=potencia/factorial; //añadimos al resulado cuando x^0/0!	
 			} else { //si mayor de 0
 				
+				potencia*=x; //se eleva a x tantas veces como n
+				factorial*=i; //acumulamos el valor del factorial ya que va aumentando cada vuelta (1,1,2,6,24...)
 				resultado+=potencia/factorial; //sumamos al resultado anterior el valor nuevo
 				// System.out.println(potencia/factorial); para ir viendo valores bien
 			}
 		}
 		
 		JOptionPane.showMessageDialog(null, "Resultado de la serie:\n" + resultado,"Resultado",JOptionPane.INFORMATION_MESSAGE);
+		
 	}
 
 }
